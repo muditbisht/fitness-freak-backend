25cca6e259fb0d2ac4ae3d6e4d6e885499af44a5
Mudit Bisht <muditsinghbisht@gmail.com>
2024-04-14 20:00:10 +0530

    update deployment

--- scripts/dev_run.sh
+++ scripts/dev_run.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-yarn start
+
+# Setup bashrc for npm and pm2
+chmod a+x ~/.bashrc &&
+PS1='$ ' &&
+source ~/.bashrc &&
+
+# Now we have got the access of EC2 and we will start the deploy .
+cd /home/ubuntu/server &&
+git checkout main &&
+git fetch --all &&
+git reset --hard origin/main &&
+git pull origin main &&
+npm ci &&
+pm2 stop ./src/server.js &&
+pm2 start ./src/server.js
