02cc7dd7fecabfba2e939b27a3b3091d5d5dad6b
Mudit Bisht <muditsinghbisht@gmail.com>
2024-06-22 16:49:42 +0530

    update NODE_ENV for server

--- scripts/run_command.sh
+++ scripts/run_command.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Setup bashrc for npm and pm2
+PS1='$ ' &&
+source ~/.bashrc &&
+
 # Now we have got the access of EC2 and we will start the deploy .
+NODE_ENV=production
 npm ci &&
 pm2 stop ./src/server.js &&
 pm2 start ./src/server.js
